98770df148fff7744ca53a289e1f03b642d7d95b
Vladimir <cool.deds@mail.ru>
2024-07-11 14:20:49 +0300

    feat: create navbar page

---
+++ src/pages/nav-bar/NavBar.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Button, Col, Dropdown, Layout, Menu, MenuProps, Row, Space } from 'antd';
+import logo from '../../shared/images/logo.svg';
+import { DownOutlined } from "@ant-design/icons";
+import './style.css';
+
+const { Header } = Layout;
+
+const NavBar = () => {
+    const items: MenuProps['items'] = [
+        {label: <a href="https://www.midjourney.com/">Midjourney</a>, key: '0',},
+        {label: <a href="https://chatgpt.com/">Chat GPT</a>, key: '1',
+        },
+        {type: 'divider'},
+    ];
+
+    const menuItems = [
+        { key: '1', label: 'Русский' },
+        { key: '2', label: 'Английский' }
+    ];
+
+    const languageMenu = (
+        <Menu items={menuItems} />
+    );
+
+    return (
+        <Layout>
+            <Header>
+                <Row justify="space-between" align="middle">
+                    <Col>
+                        <img src={logo} alt="logo" style={{ height: '32px', marginRight: '16px', verticalAlign: -10 }} />
+                    </Col>
+                    <Col flex="auto">
+                        <Menu theme="dark" mode="horizontal">
+                            <Menu.Item key="products">
+                                <Dropdown overlayStyle={{padding: 20}} menu={{ items }} trigger={['click']}>
+                                    <a className="custom-dropdown-menu" onClick={(e) => e.preventDefault()}>
+                                        <Space>
+                                            Продукты
+                                            <DownOutlined />
+                                        </Space>
+                                    </a>
+                                </Dropdown>
+                            </Menu.Item>
+                            <Menu.Item key="packages">
+                                <a className="custom-dropdown-menu">Пакеты</a>
+                            </Menu.Item>
+                            <Menu.Item key="api">
+                                <a className="custom-dropdown-menu">API</a>
+                            </Menu.Item>
+                            <Menu.Item key="blog">
+                                <a className="custom-dropdown-menu">Блог</a>
+                            </Menu.Item>
+                        </Menu>
+                    </Col>
+                    <Col>
+                        <Space size="middle">
+                            <Dropdown overlay={languageMenu} trigger={['click']}>
+                                <a className="custom-dropdown-menu" onClick={(e) => e.preventDefault()}>
+                                    <Space>
+                                        RU
+                                        <DownOutlined />
+                                    </Space>
+                                </a>
+                            </Dropdown>
+                            <Button type="primary">Авторизация</Button>
+                        </Space>
+                    </Col>
+                </Row>
+            </Header>
+        </Layout>
+    );
+};
+
+export default NavBar;

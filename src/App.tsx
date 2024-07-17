ce0251376e36da9c942822bc93e95341a2e07b70
Vladimir <cool.deds@mail.ru>
2024-07-17 13:44:28 +0300

    dev 1.1: do UI and add api for chatGPT

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import NavBar from "./pages/nav-bar/NavBar";
 import { Outlet } from "react-router-dom";
 
 function App() {
+    // it is time to dev!
     return (
         <>
             <NavBar />
